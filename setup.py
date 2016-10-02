54206614a5e23ac03bada363cb78253e97f20ddd
Chengzhu Zhang <zhang40@aztec2.llnl.gov>
2016-10-02 11:47:45 -0700

    New LICENSE and setup files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ARM Diagnoistic Package',
+      version='1.0',
+      description='An ARM data-oriented package for climate model evaluation',
+      url='https://github.com/ARM-DOE/arm-gcm-diagnostics',
+      author='Chengzhu Zhang and Shaocheng Xie',
+      author_email='zhang40@llnl.gov and  xie2@llnl.gov',
+      license='LLNL and ARM',
+      packages=['ARMDiag'],
+      zip_safe=False)
