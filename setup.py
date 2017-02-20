c2b4185a3eac720be2ba64d257bff1f65d4e4bb3
Zhang <zhang40@zhangml.llnl.gov>
2017-02-20 15:43:02 -0800

    add arm_diags folder, edit setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import find_packages,setup
 
 setup(name='ARM Diagnoistic Package',
-      version='1.0',
+      version='2.0',
       description='An ARM data-oriented package for climate model evaluation',
       url='https://github.com/ARM-DOE/arm-gcm-diagnostics',
-      author='Chengzhu Zhang and Shaocheng Xie',
-      author_email='zhang40@llnl.gov and  xie2@llnl.gov',
+      author='Chengzhu Zhang, Zeshawn Shaheen and Shaocheng Xie',
+      author_email='zhang40@llnl.gov, shaheen2@llnl.gov and  xie2@llnl.gov',
       license='LLNL and ARM',
-      packages=['ARMDiag'],
+      packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"]),
       zip_safe=False)
