00886786463d3b71f6d8c3132ace997204a4c469
Zhang <zhang40@zhangml.llnl.gov>
2017-02-21 09:03:48 -0800

    add arm_diags

---
+++ arm_diags/basicparameter.py
@@ -0,0 +1,18 @@
+
+
+test_mod = 'CESM1-CAM5'
+
+case_id = 'CESM1-CAM5_arm_diag_test'  #create folder to save diagnostics results 
+
+
+base_path = '/Users/zhang40/Documents/AIMS/arm_diags/arm_diags/'
+
+mod_path = base_path+'model'
+
+obs_path = base_path+'observation'
+
+cmip_path =  base_path+'cmip'
+
+output_path = base_path+case_id+'/output'
+
+
