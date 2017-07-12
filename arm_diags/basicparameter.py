8f0ee434734a87f4a273342204ed205d3484717a
chengzhuzhang <zhang40@llnl.gov>
2017-07-12 12:40:33 -0700

    upload release documentation

--- arm_diags/basicparameter.py
+++ arm_diags/basicparameter.py
@@ -1,8 +1,9 @@
 
-test_data_set = 'CESM1-CAM5'
+test_data_set = 'test_data'        #name of the model, which should be included as the /model/test_data_xxxx.nc
 
-case_id = 'test_arm_diag_test_all'  #create folder to save diagnostics results 
+case_id = 'test_all'  #create folder to save diagnostics results 
 
+# set input path 
 base_path = '/Users/zhang40/Documents/ARM_LLNL/repo/arm-gcm-diagnostics/arm_diags/'
 
 test_data_path = base_path+'model'
@@ -11,6 +12,7 @@ obs_path = base_path+'observation'
 
 cmip_path =  base_path+'cmip'
 
+# set output path
 output_path = base_path+case_id
 
 
