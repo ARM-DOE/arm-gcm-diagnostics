1671a9f0a74df33e338ff5f43b6690ae86dc6468
chengzhuzhang <zhang40@llnl.gov>
2019-01-11 16:24:26 -0800

    fix diurnal cycle error

--- arm_diags/src/diurnal_cycle.py
+++ arm_diags/src/diurnal_cycle.py
@@ -76,7 +76,8 @@ def diurnal_cycle_data(parameter):
     # Calculate for observational data
     obs_var_season=np.empty([len(variables),24])*np.nan
     #obs_file = glob.glob(os.path.join(obs_path,'*ARMdiag_domain_diurnal*.nc')) #read in diurnal test data
-    obs_file = glob.glob(os.path.join(obs_path,'*ARMdiag_c1_diurnal*.nc')) #read in diurnal test data
+    #obs_file = glob.glob(os.path.join(obs_path,'*ARMdiag_c1_diurnal*.nc')) #read in diurnal test data
+    obs_file = glob.glob(os.path.join(obs_path,'*ARMdiag_c1_diurnal_climo_sgp_localtime.nc')) #read in diurnal test data
     print 'ARM data'
     fin = cdms2.open(obs_file[0])
     for j, variable in enumerate(variables): 
