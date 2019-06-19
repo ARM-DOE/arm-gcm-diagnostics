add unit conversion for pr and prw, fix a bug

arm_diags/src/seasonal_mean.py
@@ -76,7 +76,7 @@ def seasonal_mean_table(parameter):
         print obs_file
         fin = cdms2.open(obs_file[0]) 
         for j, variable in enumerate(variables): 
-            try:
+            #try:
                var = fin (variable) 
                print var.shape
                
@@ -85,12 +85,17 @@ def seasonal_mean_table(parameter):
                obs_var_season[j,1:] = np.nanmean(np.reshape(var, (4,3)),axis=1)
                if variable == 'tas':
                    obs_var_season[j,1:] = obs_var_season[j,1:] -273.15
-               obs_var_season[j,0] = np.nanmean(var,axis=0)
+               if variable == 'pr':
+                   obs_var_season[j,1:] = obs_var_season[j,1:] * 24.0
+               if variable == 'prw':
+                   obs_var_season[j,1:] = obs_var_season[j,1:] * 10.0
+               obs_var_season[j,0] = np.nanmean(obs_var_season[j,1:])
+               print obs_var_season
                 
                #var24 = np.concatenate((var,var),axis=0)
                
-            except:
-                print (variable+" not processed for obs")
+            #except:
+            #    print (variable+" not processed for obs")
         fin.close() 
          
    