92416735ea6e4c638e273ed7e1e7c78c0e0dbeba
bfiesel <28496108+bfiesel@users.noreply.github.com>
2020-07-30 14:27:55 -0400

    Update diurnal_cycle.py

--- arm_diags/src/diurnal_cycle.py
+++ arm_diags/src/diurnal_cycle.py
@@ -7,7 +7,7 @@ from numpy import genfromtxt
 import csv
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import grid
-from .varid_dict import varid_longname
+from varid_dict import varid_longname
 import cdtime
 from scipy.optimize import curve_fit
 
