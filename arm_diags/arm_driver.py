69042ea076e4d7709e4e9b255b320c9fa9134afd
chengzhuzhang <zhang40@llnl.gov>
2020-09-30 15:09:06 -0700

    bring convection onset py3 compatible

--- arm_diags/arm_driver.py
+++ arm_diags/arm_driver.py
@@ -22,6 +22,7 @@ from src.create_htmls import annual_cycle_zt_html,diurnal_cycle_zt_html,diurnal_
 def make_parameters(basic_parameter):
     #f_data = open('examples/diags_set3.json').read()
     f_data = open('diags_all_multisites.json').read()
+    #f_data = open('examples/test_convection_onset.json').read()
     
     json_file = json.loads(f_data)
 
